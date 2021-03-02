e7c424516f993c4a2bd6d7272c9e462379ff3e36
rakiduam <spartayotros@gmail.com>
2021-03-02 17:49:04 -0300

    mover tipos de archivos

--- mover_tipo_archivo.py
+++ mover_tipo_archivo.py
@@ -1,14 +1,16 @@
-import os
+from os.path import join as juntar
 from shutil import move
 from glob import glob
 
-def mover_archivos(tipo, origen_carpeta, destino_carpeta):
+def mover_archivos(tipos, origen, destino):
     """
-
-    :param tipo:
-    :param origen_carpeta:
-    :param destino_carpeta:
+    :param tipos: lista en cadena de archivos a mover
+    :param origen: directorio de origen
+    :param destino: directorio de salida
     :return:
     """
-    listado_origen = glob('*' + tipo)
+    for tipo in tipos:
+        listado_origen = glob(juntar(origen, '**/*.' + tipo))
+        [move(juntar(archivo), juntar(destino, archivo.split('/')[-1])) for archivo in listado_origen]
 
+# mover_archivos(['tif', 'hdr', 'tfw'], '/home/fanr/SIG/DEM/SRTM/', '/home/fanr/SIG/DEM/SRTM/todos/')
